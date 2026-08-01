485b062861bd87c6ce948801fe47d5494c1cb0a4
Alexander Cano <cano.alexander09@gmail.com>
2026-08-01 09:01:42 -0600

    chore: scaffold Angular 17 app with PrimeNG and Sakai layout

---
+++ frontend/scrumboard-web/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import {AppLayoutComponent} from "./layout/app.layout.component";
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+
+    ]
+  }
+];
