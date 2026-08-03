4cb1236bb64940b0e80615e008f85e2954b61a54
Alexander Cano <cano.alexander09@gmail.com>
2026-08-03 08:37:43 -0600

    feature(web): clean unused import

--- frontend/scrumboard-web/src/app/app.config.ts
+++ frontend/scrumboard-web/src/app/app.config.ts
@@ -2,9 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { HttpInterceptorFn, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {authInterceptor} from "./core/auth/auth.interceptor";
 
 
