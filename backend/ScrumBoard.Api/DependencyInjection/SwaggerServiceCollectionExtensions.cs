09958755d4c419317c3144a62c9015190d9146ee
Alexander Cano <cano.alexander09@gmail.com>
2026-08-03 12:09:11 -0600

    refactor(api): add authentication and controllers configuration extension method

---
+++ backend/ScrumBoard.Api/DependencyInjection/SwaggerServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+using Microsoft.OpenApi.Models;
+
+namespace ScrumBoard.Api.DependencyInjection;
+
+public static class SwaggerServiceCollectionExtensions
+{
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            // Definir el esquema de seguridad (Bearer Token)
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Ingrese el token JWT en el formato: Bearer {su_token_aqui}"
+            });
+
+            // Requerir el token en los endpoints protegidos
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
+        return services;
+
+    }
+}
