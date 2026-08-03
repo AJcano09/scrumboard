8b645194e25ef38d0ea2e61db2b84a7f318aa3d3
Alexander Cano <cano.alexander09@gmail.com>
2026-08-03 08:39:14 -0600

    feature(web): add board component

--- frontend/scrumboard-web/src/app/features/board/board.component.ts
+++ frontend/scrumboard-web/src/app/features/board/board.component.ts
@@ -1,1 +1,19 @@
+import {Component} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+
+@Component({
+  selector: "app-board",
+  standalone: true,
+  imports:
+  [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule
+  ],
+  templateUrl: "./board.component.html",
+})
 export class BoardComponent{}
