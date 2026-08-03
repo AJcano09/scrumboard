6f2e3e779c1453bb9101d6024c19da2eacba4b17
Alexander Cano <cano.alexander09@gmail.com>
2026-08-03 10:28:33 -0600

    chore(db): add migration to drop Order column from Projects table"

---
+++ backend/ScrumBoard.Infrastructure/Migrations/20260803162730_RemoveOrderFromProject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScrumBoard.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveOrderFromProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Projects");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Projects",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
