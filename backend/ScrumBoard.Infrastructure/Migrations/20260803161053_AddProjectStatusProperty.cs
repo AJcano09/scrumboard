cf078867e84ad70509eec11e0d33ad3e12f991b4
Alexander Cano <cano.alexander09@gmail.com>
2026-08-03 10:17:38 -0600

    chore(db): add EF Core migration for ProjectStatus column

---
+++ backend/ScrumBoard.Infrastructure/Migrations/20260803161053_AddProjectStatusProperty.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScrumBoard.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectStatusProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Projects",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "Projects",
+                type: "boolean",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
